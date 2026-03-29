5e188a49d9468ae50711ab704d07bf32556044fe
Samantha John <samanthajohn@Samanthas-MacBook-Air.local>
2026-03-28 20:37:41 -0400

    Make Flask port configurable via PORT env

--- app.py
+++ app.py
@@ -232,4 +232,4 @@ app = create_app()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", "5000")), debug=True)
